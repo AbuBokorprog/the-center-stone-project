490f57b564c3a8f1e16407568022d2a7f7f24dbf
Abu Bokor <abubokor1066@gmail.com>
2023-10-07 11:45:41 +0600

    Firebase install and setup

---
+++ src/Firebase/Firebase.config.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDFAQJfTQRex8wkkdJHTzwBi-aXGe1m82A",
+  authDomain: "the-center-stone.firebaseapp.com",
+  projectId: "the-center-stone",
+  storageBucket: "the-center-stone.appspot.com",
+  messagingSenderId: "955842503696",
+  appId: "1:955842503696:web:b2dccf7e8bf4875e0a9695",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,51 @@
+import React, { createContext, useEffect, useState } from "react";
+import App from "../Firebase/Firebase.config";
+
+export const authContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const auth = getAuth(App);
+  const [user, setUser] = useState(null);
+  const [loader, setLoader] = useState(true);
+
+  //   const createUser = (email, password) => {
+  //     setLoader(true);
+  //     return createUserWithEmailAndPassword(auth, email, password);
+  //   };
+  //   const signInUser = (email, password) => {
+  //     setLoader(true);
+  //     return signInWithEmailAndPassword(auth, email, password);
+  //   };
+
+  //   useEffect(() => {
+  //     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+  //       setLoader(false);
+  //       setUser(currentUser);
+  //     });
+  //     return () => {
+  //       return unSubscribe();
+  //     };
+  //   }, []);
+  //   const updateProfileData = async (Name, Image) => {
+  //     setLoader(true);
+  //     try {
+  //       await updateProfile(auth.currentUser, {
+  //         displayName: Name,
+  //         photoURL: Image,
+  //       });
+  //       setUser(auth.currentUser);
+  //     } catch (error) {
+  //       console.log("Error updating profile:", error);
+  //     } finally {
+  //       setLoader(false);
+  //     }
+  //   };
+
+  const authInfo = {};
+
+  return (
+    <authContext.Provider value={authInfo}>{children}</authContext.Provider>
+  );
+};
+
+export default AuthProvider;
