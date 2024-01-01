b1e273f008b1859d508d6310c38c93477773a551
AbuBokorprog <abubokor1066@gmail.com>
2024-01-01 14:57:45 +0600

    checkout page

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { authContext } from "../../Provider/AuthProvider";
 import { Link } from "react-router-dom";
 
-const Cart = () => {
+const Cart = ({ img, gap, payment }) => {
   const { user } = useContext(authContext);
   const [carts, setCart] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -20,7 +20,7 @@ const Cart = () => {
           (acc, item) => acc + parseFloat(item.cost) * item.quantity,
           0
         );
-        setOverallTotal(total);
+        setOverallTotal((total / 100) * 5 + total);
       } catch (error) {
         console.error("Error fetching cart data:", error);
       }
@@ -28,45 +28,6 @@ const Cart = () => {
 
     fetchCartData();
   }, [user?.email]);
-
-  const updateQuantity = async (cartId, newQuantity) => {
-    try {
-      const response = await fetch(
-        `https://center-stone-server-side.vercel.app/cart/${cartId}`,
-        {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ quantity: newQuantity }),
-        }
-      );
-
-      if (response.ok) {
-        setCart((prevCarts) =>
-          prevCarts.map((cart) =>
-            cart._id === cartId ? { ...cart, quantity: newQuantity } : cart
-          )
-        );
-      } else {
-        console.error("Failed to update quantity on the server");
-      }
-    } catch (error) {
-      console.error("Error updating quantity:", error);
-    }
-  };
-
-  const plusHandler = (cartId, currentQuantity) => {
-    const newQuantity = currentQuantity + 1;
-    updateQuantity(cartId, newQuantity);
-  };
-
-  const minusHandler = (cartId, currentQuantity) => {
-    if (currentQuantity > 0) {
-      const newQuantity = currentQuantity - 1;
-      updateQuantity(cartId, newQuantity);
-    }
-  };
 
   const deleteHandler = (email, title) => {
     fetch(
@@ -96,16 +57,24 @@ const Cart = () => {
   };
 
   return (
-    <div className="pb-20">
-      <h1 className="text-3xl mb-6 font-bold">Your Cart</h1>
+    <div className="pb-10 lg:pb-20">
+      {/* <h1 className="text-3xl mb-6 font-bold">Your Cart</h1> */}
 
       {carts.map((c) => (
         <div key={c._id}>
-          <div className="flex gap-10 mx-auto justify-between items-center my-2">
+          <div
+            className={`flex ${
+              gap ? gap : "gap-10"
+            } mx-auto justify-between items-center my-2`}
+          >
             <figure>
-              <img src={c.image} alt="" className="w-60 h-60" />
+              <img
+                src={c.image}
+                alt={c.title}
+                className={`${img ? img : "w-60 h-60"} `}
+              />
             </figure>
-            <h6 className="text-3xl mb-6 font-semibold w-96">{c.title}</h6>
+            <h6 className="text-3xl mb-6 w-96">{c.title}</h6>
             <div className="flex gap-4 items-center">
               <p className="font-semibold">Quantity: {c.quantity}</p>
             </div>
@@ -121,8 +90,8 @@ const Cart = () => {
         </div>
       ))}
 
-      <div>
-        <div className="my-8">
+      <div className="">
+        <div className="my-8 card">
           <div className="flex justify-between my-1">
             <h2 className="text-3xl">Total:</h2>
             <h2 className="text-3xl">${overallTotal}</h2>
@@ -133,16 +102,18 @@ const Cart = () => {
           </div>
           <div className="flex justify-between">
             <h2 className="text-3xl">Total Amount:</h2>
-            <h2 className="text-3xl text-yellow-400">
-              {(overallTotal / 100) * 5 + overallTotal}
-            </h2>
+            <h2 className="text-3xl text-yellow-400">{overallTotal}</h2>
           </div>
         </div>
-        <div className="text-right mt-10 mx-auto ">
-          <Link to={"/checkout"} className="btn btn-warning">
-            Checkout
-          </Link>
-        </div>
+        {payment ? (
+          payment
+        ) : (
+          <div className="text-right mt-10 mx-auto ">
+            <Link to={"/checkout"} className="btn btn-warning">
+              Checkout
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import Cart from "../Cart/Cart";
+import { Link } from "react-router-dom";
+import { authContext } from "../../Provider/AuthProvider";
 
 const Checkout = () => {
+  const { user } = useContext(authContext);
+  const [carts, setCart] = useState([]);
+  const [overallTotal, setOverallTotal] = useState("");
+  useEffect(() => {
+    const fetchCartData = async () => {
+      try {
+        const response = await fetch(
+          `https://center-stone-server-side.vercel.app/cart/${user?.email}`
+        );
+        const data = await response.json();
+        setCart(data);
+        const total = data.reduce(
+          (acc, item) => acc + parseFloat(item.cost) * item.quantity,
+          0
+        );
+        setOverallTotal((total / 100) * 5 + total);
+      } catch (error) {
+        console.error("Error fetching cart data:", error);
+      }
+    };
+
+    fetchCartData();
+  }, [user?.email]);
   const {
     register,
     handleSubmit,
@@ -9,61 +35,90 @@ const Checkout = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    // console.log(data, carts, overallTotal);
+    alert("Payment success!");
+  };
 
   return (
-    <div>
-      <h1>Checkout</h1>
-      <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div>
-            <label>Name</label>
-            <input
-              {...register("name", { required: true, maxLength: 20 })}
-              className="input input-bordered input-secondary w-full"
-            />
-          </div>
-          <div>
-            <label>Phone</label>
-            <input
-              type="number"
-              {...register("Phone", { required: true, maxLength: 20 })}
-              className="input input-bordered input-secondary w-full"
-            />
-          </div>
-          <div>
-            <label>Email</label>
-            <input
-              type="email"
-              {...register("email", { required: true, maxLength: 20 })}
-              className="input input-bordered input-secondary w-full"
-            />
-          </div>
-          <div>
-            <label>District</label>
-            <input
-              {...register("District", { required: true, maxLength: 20 })}
-              className="input input-bordered input-secondary w-full"
-            />
-          </div>
-          <div>
-            <label>Area</label>
-            <input
-              {...register("Area", { required: true, maxLength: 20 })}
-              className="input input-bordered input-secondary w-full"
-            />
-          </div>
-          <div>
-            <label>Address</label>
-            <input
-              {...register("Address", { required: true, maxLength: 20 })}
-              className="input input-bordered input-secondary w-full"
-            />
-          </div>
-        </form>
+    <div className="pb-10 lg:pb-20">
+      <div className="flex gap-6">
+        <div className="w-full">
+          <form>
+            <div>
+              <label>Name</label>
+              <input
+                placeholder="Type Your Name"
+                {...register("name", { required: true, maxLength: 20 })}
+                className="input input-bordered input-secondary w-full"
+              />
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <label>Phone</label>
+                <input
+                  placeholder="Type Your Phone"
+                  type="number"
+                  {...register("Phone", { required: true, maxLength: 20 })}
+                  className="input input-bordered input-secondary w-full"
+                />
+              </div>
+              <div>
+                <label>Email</label>
+                <input
+                  type="email"
+                  placeholder="Type Your Email"
+                  {...register("email", { required: true, maxLength: 20 })}
+                  className="input input-bordered input-secondary w-full"
+                />
+              </div>
+            </div>
+            <div>
+              <label>District</label>
+              <input
+                placeholder="Type Your District"
+                {...register("District", { required: true, maxLength: 20 })}
+                className="input input-bordered input-secondary w-full"
+              />
+            </div>
+            <div>
+              <label>Area</label>
+              <input
+                placeholder="Type Your Area Name"
+                {...register("Area", { required: true, maxLength: 20 })}
+                className="input input-bordered input-secondary w-full"
+              />
+            </div>
+            <div>
+              <label>Address</label>
+              <input
+                placeholder="Type Your Address"
+                {...register("Address", { required: true, maxLength: 20 })}
+                className="input input-bordered input-secondary w-full"
+              />
+            </div>
+          </form>
+        </div>
+        <div className="w-1/2">
+          <Cart
+            img={"w-20 h-20"}
+            gap={"gap-4"}
+            payment={
+              <div className="text-right mt-10 mx-auto ">
+                <Link
+                  onClick={handleSubmit(onSubmit)}
+                  className="btn btn-warning"
+                >
+                  Payment
+                </Link>
+              </div>
+            }
+          />
+        </div>
       </div>
     </div>
   );
 };
+// console.log(data)
 
 export default Checkout;
